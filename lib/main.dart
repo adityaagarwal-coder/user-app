55eae7c239617f867625fb3b7dc0bd82030f971f
adityaagarwal-coder <82513880+adityaagarwal-coder@users.noreply.github.com>
2021-10-05 22:12:25 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: {MyRoutes.signuproute: (context) => Home()},
+      routes: {MyRoutes.homeroute: (context) => Home()},
     );
   }
 }
